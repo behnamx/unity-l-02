8be9626d5c951b3d3f478282b16c475fc86ea5bf
behnamx <behnamx@icloud.com>
2017-01-25 11:53:02 -0800

    Added Scene Management and Collision events

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Goal : MonoBehaviour {
+
+    // Use this for initialization
+
+    public string nextLevel = null;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Meteor meteor = other.gameObject.GetComponent<Meteor>();
+        if (meteor != null)
+        {
+            if (nextLevel != null && nextLevel != "")
+            {
+                SceneManager.LoadScene(nextLevel);
+            }
+            meteor.OnDestroy(); //so if we don't load a level we keep playing the current one
+        }
+    }
+}
---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Meteor meteor = other.gameObject.GetComponent<Meteor>();
+        if (meteor != null)
+        {
+            meteor.OnDestroy();
+        }
+    }
+
+
+}
